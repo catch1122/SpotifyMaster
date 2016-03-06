c8d70063b04fb503b85d46ccab9af97f6fc4086c
Brian Zhang <bzhang1670@gmail.com>
2016-03-05 22:16:35 -0800

    start query Music basics

---
+++ SpotifyMaster/app/src/main/java/com/example/kwonjoanne/spotifymaster/ScrollingActivity.java
@@ -0,0 +1,38 @@
+package com.example.kwonjoanne.spotifymaster;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.util.ArrayList;
+
+
+public class ScrollingActivity extends AppCompatActivity {
+
+    public void queryMusic(String query, ArrayList<String> results) //not sure if correct return yet
+    {
+        //user library querying function
+        //put them into results.
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
